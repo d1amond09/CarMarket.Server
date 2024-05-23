ebd8f3c8e8d207cd24f99ba2458536c29a66b111
Maksim Berdnikau <max.berd.1900@gmail.com>
2024-05-23 15:03:27 +0300

    Creation configuration for Services and WebApplication

--- CarMarket.Server/Program.cs
+++ CarMarket.Server/Program.cs
@@ -1,27 +1,53 @@
-namespace CarMarket.Server
+using CarMarket.Server.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
+namespace CarMarket.Server;
+
+public class Program
 {
-	public class Program
+	public static void Main(string[] args)
 	{
-		public static void Main(string[] args)
+		var builder = WebApplication.CreateBuilder(args);
+
+		ConfigureServices(builder.Services);
+
+		var app = builder.Build();
+
+		ConfigureApp(app);
+
+		app.Run();
+	}
+
+	public static void ConfigureServices(IServiceCollection services)
+	{
+		services.ConfigureCors();
+		services.ConfigureIISIntegration();
+		services.AddControllers();
+	}
+
+	public static void ConfigureApp(WebApplication app)
+	{
+		if (app.Environment.IsDevelopment())
 		{
-			var builder = WebApplication.CreateBuilder(args);
-
-			// Add services to the container.
-
-			builder.Services.AddControllers();
-
-			var app = builder.Build();
-
-			// Configure the HTTP request pipeline.
-
-			app.UseHttpsRedirection();
-
-			app.UseAuthorization();
-
-
-			app.MapControllers();
-
-			app.Run();
+			app.UseDeveloperExceptionPage();
 		}
+		else
+		{
+			app.UseHsts();
+		}
+		app.UseHttpsRedirection();
+		app.UseStaticFiles();
+		app.UseCors("CorsPolicy");
+		app.UseForwardedHeaders(new ForwardedHeadersOptions
+		{
+			ForwardedHeaders = ForwardedHeaders.All
+		});
+		app.UseRouting();
+		app.UseAuthorization();
+
+		app.MapRazorPages();
+		app.MapControllerRoute(
+			name: "default",
+			pattern: "{controller=Home}/{action=Index}/{id?}");
 	}
 }
